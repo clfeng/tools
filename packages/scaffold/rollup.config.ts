1e0644df2be2502f815e8024c2c1ace4bb9714a6
clfeng <9331500-clfeng1@users.noreply.gitlab.com>
2023-02-26 18:15:35 +0800

    feat: 初始化项目

---
+++ packages/scaffold/rollup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'rollup';
+import typescript from 'rollup-plugin-typescript2';
+
+export default defineConfig({
+  input: 'src/index.ts',
+	output: {
+    dir: "dist",
+		format: 'cjs'
+	},
+  plugins: [
+    typescript()
+  ],
+  watch: {
+    include: ["./src"]
+  }
+});
